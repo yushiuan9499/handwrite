70916f8eb2ba2aeaf8d1f46a33b456d7144ccb03
yushiuan9499 <yushiuan9499@gmail.com>
2025-05-15 21:14:08 +0800

    fix: write main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+import utils.gui
+import sys
 def main():
     print("Hello from handwrite!")
+    # Initialize the GUI
+    app = utils.gui.QApplication(sys.argv)
+    gui = utils.gui.HandwritingGUI()
+    gui.show()
+    # Execute the application
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
